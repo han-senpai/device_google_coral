25c82bec6aea5a63e75dca2a7c19f432723d64e8
han-senpai <snfhsn8@gmail.com>
2025-11-13 09:59:40 +0700

    Delete device-evolution.mk

--- device-evolution.mk
+++
@@ -1,8 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2025 The Evolution X Project
-# SPDX-License-Identifier: Apache-2.0
-#
-DEVICE_PACKAGE_OVERLAYS += device/google/coral/overlay-evolution
-
-# Use MINI GApps
-TARGET_USES_MINI_GAPPS := true
