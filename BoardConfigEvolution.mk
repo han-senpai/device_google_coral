f922034cafbd153c1889d0b8ec7e99de1893a655
han-senpai <snfhsn8@gmail.com>
2025-11-13 10:00:22 +0700

    Delete BoardConfigEvolution.mk

--- BoardConfigEvolution.mk
+++
@@ -1,7 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2025 The Evolution X Project
-# SPDX-License-Identifier: Apache-2.0
-#
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
