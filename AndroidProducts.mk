9d7fda7011993e7c03e7dde2b6a6b9ce6400fcc0
han-senpai <snfhsn8@gmail.com>
2025-11-13 10:03:55 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -26,10 +26,10 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
     $(LOCAL_DIR)/lineage_coral.mk \
-    $(LOCAL_DIR)/lineage_flame.mk
+    $(LOCAL_DIR)/infinity_flame.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_coral-trunk_staging-userdebug \
     aosp_flame-trunk_staging-userdebug \
     lineage_coral-trunk_staging-userdebug \
-    lineage_flame-trunk_staging-userdebug
+    infinity_flame-trunk_staging-userdebug
